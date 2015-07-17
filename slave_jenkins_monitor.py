2f6b2022e6f1699c1aa5555475b4cdd283d4ef0e
paulo ledo <paulofledo@gmail.com>
2015-07-17 20:59:37 -0300

    Refazendo com oo

--- slave_jenkins_monitor.py
+++ slave_jenkins_monitor.py
@@ -3,26 +3,35 @@
 
 from requests import get
 from sys import exit
-
+import json
 #------------- Variables
 
-ci_api_url = 'http://ci.knowledge4.life:8080/computer/api/json'
-
+#ci_api_url = 'http://ci.knowledge4.life:8080/computer/api/json'
+ci_api_url = 'http://localhost:1235/computer/api/json'
+slaves_required = ['njr_android_slave_new', 'njr_api_slave_new', 'njr_ios_slave' ]
 
 
 #------------- Functions
 
+
+#-------------------------------
 def get_jenkins_api_json(url_computer_api_json):
     """docstring for get_jenkins_api_json"""
 #    print("Inside in the get_jenkins_api_jso function")
     jenkins_api_json = get(url_computer_api_json)
     return(jenkins_api_json)
 
-def tratando_json(api_json_raw):
+def get_nodes_slave(api_json_raw):
     """docstring for treating_json"""
     print("Inside in the tratando_json function")
     json_treated = api_json_raw.json()
-    return(json_treated)
+    nodes_slave = json.dumps(json_treated)
+    return(nodes_slave)
+
+def verify_nodes_slave(nodes_slave_list):
+    """verify status offline _nodes slave """
+    print("Inside in the verify_nodes_slave function")
+    #for node in nodes_slave_list
 
 def main():
     """docstring for main"""
--- tests/test_slave_jenkins_monitor.py
+++ tests/test_slave_jenkins_monitor.py
@@ -1,15 +1,16 @@
 import pytest
 from  .. import slave_jenkins_monitor
 
+jenkins_url = "http://localhost:1235/computer/api/json"
+
 def test_get_jenkins_api_json():
-    url_jenkins_json = 'http://ci.knowledge4.life:8080/computer/api/json'
+    url_jenkins_json = jenkins_url
     status_code = slave_jenkins_monitor.get_jenkins_api_json(url_jenkins_json)
     assert status_code.status_code == 200
 
-def test_tratrando_json():
+def test_get_nodes_slave():
     """test api_json_treated funtion"""
-    url_jenkins_json = 'http://ci.knowledge4.life:8080/computer/api/json'
-    json_obj_jenkins = slave_jenkins_monitor.get_jenkins_api_json(url_jenkins_json)
-    json_tratado = slave_jenkins_monitor.tratando_json(json_obj_jenkins)
-    result_tratamento = isinstance(json_tratado, dict)
+    json_obj_jenkins = slave_jenkins_monitor.get_jenkins_api_json(jenkins_url)
+    nodes_slave_list = slave_jenkins_monitor.get_nodes_slave(json_obj_jenkins)
+    result_tratamento = isinstance(nodes_slave_list, str)
     assert result_tratamento == True
